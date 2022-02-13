f6eee23e78f6162d241a02f60182c1c9a9506c33
Curtis-Stilwell <stilcurt@yahoo.com>
2022-02-13 09:12:21 -0600

    Cucumber Tutorial.

---
+++ src/stepdefinition/TestingSteps.java
@@ -0,0 +1,43 @@
+package stepdefinition;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TestingSteps {
+static WebDriver driver;
+	
+@Given("User is on Home Page")
+public void user_is_on_home_page() {
+    // Write code here that turns the phrase above into concrete actions
+  
+}
+@When("User enters Username and Password")
+public void user_enters_username_and_password() {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
+@When("Clicks Go button")
+public void clicks_go_button() {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
+@Then("He can visit the practice page")
+public void he_can_visit_the_practice_page() {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
+@Then("A message is displayed")
+public void a_message_is_displayed() {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+}
